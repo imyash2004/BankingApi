3f3b21ba2b00e864a7ac9dcbbefab67f7d0b3f8b
imyash2004 <agarwalaysh041@gmail.com>
2025-02-25 23:56:32 +0530

    Added a demo fib function in yash.java

--- src/main/java/com/bankingapi/Yash.java
+++ src/main/java/com/bankingapi/Yash.java
@@ -2,6 +2,21 @@ package com.bankingapi;
 
 public class Yash {
     public static void main(String[] args) {
+
+
         System.out.println("yahs");
+    }
+    public static void prinFibbhSeries(int count) {
+        int num1 = 0, num2 = 1;
+
+        System.out.println("Fibcci ]tfrioeries up to " + count + " numbers:");
+
+        for (int i = 1; i <= count; i++) {
+            System.out.println(num1 + " ");
+
+            int nextNum = num1 + num2;
+            num1 = num2;
+            num2 = nextNum;
+        }
     }
 }
